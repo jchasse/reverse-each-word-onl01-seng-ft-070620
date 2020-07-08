89e16f362608483d0d10deaf02a6685b5180d22f
Jonathan Chasse <jonathan.r.chasse@gmail.com>
2020-07-08 03:03:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,5 +3,5 @@ def reverse_each_word(string)
   reverse_string = string.reverse
   array = reverse_string.split(" ")
   reverse_array = array.reverse
-  puts reverse_array.join(" ")
+  return reverse_array.join(" ")
 end
